a93baca1f4aef80d8a7f0d3325801a95fd7e1f17
Raja Farhan Zafar <bcs163997@cust.pk>
2020-04-22 01:13:34 +0500

    Lab7 task

--- src/filters/FilterAuth.java
+++ src/filters/FilterAuth.java
@@ -5,22 +5,3 @@ import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-
-@WebFilter(filterName = "FilterAuth")
-public class FilterAuth implements Filter {
-    public void destroy() {
-    }
-
-    public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws ServletException, IOException {
-        if(((HttpServletRequest)req).getSession().getAttribute("uemail")==null){
-            ((HttpServletResponse)resp).sendRedirect("login.xhtml");
-        }else{
-            chain.doFilter(req, resp);
-        }
-    }
-
-    public void init(FilterConfig config) throws ServletException {
-
-    }
-
-}
